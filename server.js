3ef72ede91bc02bc8e1972359ad8d526f4e2d226
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-06-30 21:15:48 +0200

    feat: remove all commas
    fixes #3

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get('/', (req, res) => res.send(`Hello World!`));
+app.get('/', (req, res) => res.send(`Hello World!`))
 
-app.get('/hello2', (req, res) => res.send(`Hello World2!`));
+app.get('/hello2', (req, res) => res.send(`Hello World2!`))
 
 app.listen(port, () => console.log(
   `Example app listening at http://localhast:${port}`,
-));
+))
