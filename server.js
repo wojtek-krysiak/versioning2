ae24d1aebca9c1b03fdd8084018bb173dd862676
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-06-30 21:11:37 +0200

    feat: new route

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send(`Hello World!`));
 
+app.get('/hello2', (req, res) => res.send(`Hello World2!`));
+
 app.listen(port, () => console.log(
   `Example app listening at http://localhast:${port}`,
 ));
