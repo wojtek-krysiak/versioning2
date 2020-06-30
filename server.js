d67e8714fc84f6217372259efeec5e26d89fe497
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-06-30 15:30:12 +0200

    feat: add new release things

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send(
+  `Hello World! v: ${process.env.npm_package_version}`),
+);
 
 app.listen(port, () => console.log(
-  `Example app listening at http://localhast:${port}`
+  `Example app listening at http://localhast:${port}`,
 ));
