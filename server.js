36a0391bb48e044f806812e4eced6783be77bdae
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-07-29 14:53:21 +0200

    chore: bring back old version

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const port = 3000
 
 app.get('/', (req, res) => res.send(`Hello World!`))
 
-app.get('/hello2', (req, res) => res.send(`Hello Worlds!`))
-app.get('/hello3', (req, res) => res.send(`Hello Worlds!`))
-
 app.listen(port, () => console.log(
   `Example app listening at http://localhast:${port}`,
 ))
