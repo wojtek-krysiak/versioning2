7d916018a7043a17d63796dbd664f7411fe9a09a
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-07-29 14:24:52 +0200

    fix: typo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 3000
 
 app.get('/', (req, res) => res.send(`Hello World!`))
 
-app.get('/hello2', (req, res) => res.send(`Hello World2!`))
+app.get('/hello2', (req, res) => res.send(`Hello Worlds!`))
 
 app.listen(port, () => console.log(
   `Example app listening at http://localhast:${port}`,
